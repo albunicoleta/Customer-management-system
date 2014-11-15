87abdb538e2d1658b323a1dcaab660911225bde7
Nicoleta <albunicoleta@gmail.com>
2014-11-15 16:43:30 +0200

    adding admin controller

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+class AdminController extends BaseController {
+
+    /**
+     * create save action for login
+     */
+    public function postLogin()
+    {
+        $input = Input::only('username', 'password');
+        if (Auth::attempt(array('username' => $input['username'], 'password' => $input['password']))) {
+            /* @todo redirect to a proper route */
+            return Redirect::intended('dashboard');
+        }
+        
+        return Redirect::intended('/admin');
+    }
+
+}
