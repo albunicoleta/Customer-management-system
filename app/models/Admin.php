86f75bef0899abccd5d9bf423c058342d1d1ec1c
Nicoleta <albunicoleta@gmail.com>
2014-11-15 16:26:39 +0200

    adding model

---
+++ app/models/Admin.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Admin extends Eloquent implements UserInterface, RemindableInterface {
+
+	use UserTrait, RemindableTrait;
+        
+        const TABLE_NAME = 'admin_users';
+        
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = self::TABLE_NAME;
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password', 'remember_token');
+
+}
+
+
