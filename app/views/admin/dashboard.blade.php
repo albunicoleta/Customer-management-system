3e22c9f23cf567b6fcb9a3ac5bf2a9e32e1ae531
Nicoleta <albunicoleta@gmail.com>
2014-11-17 21:46:55 +0200

    resolves #4

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -1,8 +1,59 @@
 @extends('layout')
 
 @section('content')
-<div class="form-horizontal">
-    <p>this is the dashboard</p>
+<nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Users</a></li>
+                <li><a href="#">User groups</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+<div class="container jumbotron" role="main">
+    <div class="page-header">
+        <h1>Users</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>    
+                        <td>Nicoleta</td>
+                        <td>Albu</td>
+                        <td>Galati</td>
+                        <td>123456789</td>
+                        <td>albunicoleta@gmail.com</td>
+                        <td>Galati</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+
+<script type="text/javascript" src="jquery_1.6.1.js"></script>
+<script type="text/javascript">
+
+</script>
 @stop
 
