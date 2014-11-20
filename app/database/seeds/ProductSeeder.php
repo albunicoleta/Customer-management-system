95666d6230ae152cb33c33e00a49bce29d116c57
Nicoleta <albunicoleta@gmail.com>
2014-11-20 19:03:24 +0200

    assign random products to random customers

--- app/database/seeds/ProductSeeder.php
+++ app/database/seeds/ProductSeeder.php
@@ -15,8 +15,26 @@ class ProductSeeder extends Seeder {
         $newProduct = new Product();
         $newProduct->fill(array('name' => 'book'));
         $newProduct->save();
-        $customer = Customer::all()->first();
+        
+        $newProduct2 = new Product();
+        $newProduct2->fill(array('name' => 'toy'));
+        $newProduct2->save();
+        
+        $newProduct3 = new Product();
+        $newProduct3->fill(array('name' => 'Computer'));
+        $newProduct3->save();
+        
+        $customers = Customer::all();
+        $customer = $customers->first();
         $newProduct->customers()->sync(array($customer->id));
+        $newProduct2->customers()->sync(array($customer->id));
+        
+        $customer = $customers->random();
+        $newProduct2->customers()->sync(array($customer->id));
+        
+        $customer = $customers->last();
+        $newProduct->customers()->sync(array($customer->id));
+        $newProduct3->customers()->sync(array($customer->id));
     }
 
 }
